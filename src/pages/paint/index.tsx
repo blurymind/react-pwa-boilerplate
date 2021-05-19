705a54a0c7eb925663dea6fe6467874b7aa2fb21
Todor Imreorov <blurymind@gmail.com>
2021-05-19 19:38:26 +0100

    save chi file to cache

--- src/pages/paint/index.tsx
+++ src/pages/paint/index.tsx
@@ -4,6 +4,7 @@ import {
   getCacheKeys,
   storeCache,
   binaryStringToByteArray,
+  getCacheData,
 } from "@helpers/cache";
 import useLocalStorage from "@hooks/use-local-storage";
 
@@ -34,79 +35,106 @@ export function Paint() {
   };
   useEffect(() => {
     setTimeout(() => {
-      chickRef.current = new ChickenPaint({
-        uiElem: document.getElementById("chickenpaint-parent"),
-        // saveUrl: "save.php",
-        // postUrl: "complete.php",
-        // exitUrl: "index.php",
-        // resourcesRoot: "chickenpaint/",
-      });
-      setTimeout(() => {
-        const toRemove = document.getElementsByClassName("navbar-brand")[0];
-        toRemove.remove();
-        const nameInput = document.createElement("input");
-        nameInput.id = "fileNameInput";
-        nameInput.placeholder = "new";
-        nameInput.value = currentChick;
+      getCacheKeys((storedFiles: any) => {
+        console.log(currentChick, storedFiles);
 
-        document
-          .getElementsByClassName(
-            "navbar navbar-expand-md navbar-light bg-light"
-          )[0]
-          .appendChild(nameInput);
-        nameInput.addEventListener("input", (event: any) => {
-          console.log(event.target.value);
-          setCurrentChick(event.target.value);
-        });
-
-        const saveButton = document.createElement("button");
-        saveButton.id = "saveBtn";
-        saveButton.textContent = "save";
-        saveButton.onclick = () => {
-          console.log(chickRef.current, chickRef.current?.getArtwork());
-          const flat = binaryStringToByteArray(
-            chickRef.current?.getArtwork().getFlatPNG()
-          );
-          const flatBlob = new Blob([flat], { type: "image/png" });
-          const blobUrl = URL.createObjectURL(flatBlob);
-          // console.log(blob, blobUrl);
-          storeCache(
-            (nameInput.value || nameInput.placeholder || "chickenPaint") +
-              ".png",
-            blobUrl
-          );
-
-          console.log(chickRef.current?.getArtwork());
-          // save chi file
-          save(chickRef.current?.getArtwork()).then((chibiResult: any) => {
-            console.log("Layered:", chibiResult);
+        let loadChiOrPngUrl = "";
+        if (storedFiles.includes(currentChick + ".chi")) {
+          // alert("Has been saved");
+          // attempt to load it into chickenpaint from cache
+          console.log("Attempting to load", currentChick + ".chi");
+          getCacheData(currentChick + ".chi", (dataUri: string) => {
+            console.log("DATAuri", dataUri);
+            chickRef.current = new ChickenPaint({
+              uiElem: document.getElementById("chickenpaint-parent"),
+              loadChibiFileURL: dataUri, // <- doesnt seem to like this url
+              // saveUrl: "save.php",
+              // postUrl: "complete.php",
+              // exitUrl: "index.php",
+              // resourcesRoot: "chickenpaint/",
+            });
+            // monogatari.assets("scenes", cachedScenes);
           });
-        };
-        document
-          .getElementsByClassName(
-            "navbar navbar-expand-md navbar-light bg-light"
-          )[0]
-          .appendChild(saveButton);
+        } else {
+          chickRef.current = new ChickenPaint({
+            uiElem: document.getElementById("chickenpaint-parent"),
+            // saveUrl: "save.php",
+            // postUrl: "complete.php",
+            // exitUrl: "index.php",
+            // resourcesRoot: "chickenpaint/",
+          });
+        }
 
-        console.log(chickRef.current);
+        setTimeout(() => {
+          const toRemove = document.getElementsByClassName("navbar-brand")[0];
+          toRemove.remove();
+          const nameInput = document.createElement("input");
+          nameInput.id = "fileNameInput";
+          nameInput.placeholder = "new";
+          nameInput.value = currentChick;
 
-        // we could also do this on input and add it to input's autocompletion
-        getCacheKeys((storedFiles: any) => {
-          console.log(currentChick, storedFiles);
-          if (storedFiles.includes(currentChick + ".png")) {
-            // alert("Has been saved");
-            // attempt to load it into chickenpaint from cache
-          }
-        });
+          document
+            .getElementsByClassName(
+              "navbar navbar-expand-md navbar-light bg-light"
+            )[0]
+            .appendChild(nameInput);
+          nameInput.addEventListener("input", (event: any) => {
+            console.log(event.target.value);
+            setCurrentChick(event.target.value);
+          });
 
-        // restore chick Positions
-        console.log("STYLES LOADED", chickWidgetPositions);
-        Object.entries(chickWidgetPositions).forEach(([key, value]: any) => {
-          //@ts-ignore
-          document.getElementsByClassName(key)[0].style = value;
-          console.log("apply:", document.getElementsByClassName(key)[0]);
-        });
-      }, 100);
+          const saveButton = document.createElement("button");
+          saveButton.id = "saveBtn";
+          saveButton.textContent = "save";
+          saveButton.onclick = () => {
+            console.log(chickRef.current, chickRef.current?.getArtwork());
+            const flat = binaryStringToByteArray(
+              chickRef.current?.getArtwork().getFlatPNG()
+            );
+            const flatBlob = new Blob([flat], { type: "image/png" });
+            const blobUrl = URL.createObjectURL(flatBlob);
+            // console.log(blob, blobUrl);
+            storeCache(
+              (nameInput.value || nameInput.placeholder || "chickenPaint") +
+                ".png",
+              blobUrl
+            );
+
+            console.log(
+              chickRef.current?.getArtwork(),
+              chickRef.current?.artwork
+                .getLayersRoot()
+                .getLinearizedLayerList(false)
+            );
+            // save chi file
+            save(chickRef.current?.getArtwork()).then((chibiResult: any) => {
+              console.log("Layered saved:", chibiResult);
+              const blobUrl = URL.createObjectURL(chibiResult.bytes);
+              storeCache(
+                (nameInput.value || nameInput.placeholder || "chickenPaint") +
+                  ".chi",
+                blobUrl
+              );
+            });
+          };
+          document
+            .getElementsByClassName(
+              "navbar navbar-expand-md navbar-light bg-light"
+            )[0]
+            .appendChild(saveButton);
+
+          console.log(chickRef.current);
+
+          // we could also do this on input and add it to input's autocompletion
+
+          // restore chick Positions
+          // console.log("STYLES LOADED", chickWidgetPositions);
+          Object.entries(chickWidgetPositions).forEach(([key, value]: any) => {
+            //@ts-ignore
+            document.getElementsByClassName(key)[0].style = value;
+          });
+        }, 100);
+      });
     }, 100);
 
     //unmount
