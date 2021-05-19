859388ceced83e54cf0e387224541d273e7c83d8
Todor Imreorov <blurymind@gmail.com>
2021-05-19 21:17:20 +0100

    add file suggestions to open existing files

--- src/pages/paint/index.tsx
+++ src/pages/paint/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import "chickenpaint/resources/css/chickenpaint.css";
 import {
   getCacheKeys,
@@ -62,76 +62,96 @@ export function Paint() {
             canvasHeight: 200,
           });
         }
-      });
 
-      setTimeout(() => {
-        const toRemove = document.getElementsByClassName("navbar-brand")[0];
-        toRemove?.remove();
-        const nameInput = document.createElement("input");
-        nameInput.id = "fileNameInput";
-        nameInput.placeholder = "new";
-        nameInput.value = currentChick;
+        setTimeout(() => {
+          const toRemove = document.getElementsByClassName("navbar-brand")[0];
+          toRemove?.remove();
+          const nameInput = document.createElement("input");
+          nameInput.type = "list";
+          nameInput.id = "fileNameInput";
+          nameInput.placeholder = "new";
+          nameInput.value = currentChick;
 
-        document
-          .getElementsByClassName(
-            "navbar navbar-expand-md navbar-light bg-light"
-          )[0]
-          .appendChild(nameInput);
-        nameInput.addEventListener("blur", (event: any) => {
-          console.log(event.target.value);
-          setCurrentChick(event.target.value);
-        });
-
-        const saveButton = document.createElement("button");
-        saveButton.id = "saveBtn";
-        saveButton.textContent = "save";
-        saveButton.onclick = () => {
-          console.log(chickRef.current, chickRef.current?.getArtwork());
-          const flat = binaryStringToByteArray(
-            chickRef.current?.getArtwork().getFlatPNG()
+          nameInput.setAttribute("list", "fileSuggestions");
+          const dataList = document.createElement("datalist");
+          dataList.id = "fileSuggestions";
+          const sanitizedStoredList = storedFiles.map((text: string) =>
+            text.replace(".png", "").replace(".chi", "")
           );
-          // save image file (for game engine)
-          const flatBlob = new Blob([flat], { type: "image/png" });
-          const blobUrl = URL.createObjectURL(flatBlob);
-          storeCache(
-            (nameInput.value || nameInput.placeholder || "chickenPaint") +
-              ".png",
-            blobUrl
-          );
+          sanitizedStoredList
+            .filter(
+              (item: string, index: number) =>
+                sanitizedStoredList.indexOf(item) === index
+            )
+            .forEach((suggestionText: string) => {
+              const suggestion = document.createElement("option");
+              suggestion.value = suggestionText;
 
-          console.log(
-            chickRef.current?.getArtwork(),
-            chickRef.current?.artwork
-              .getLayersRoot()
-              .getLinearizedLayerList(false)
-          );
-          // save chi file too
-          save(chickRef.current?.getArtwork()).then((chibiResult: any) => {
-            console.log("Layered saved:", chibiResult);
-            const blobUrl = URL.createObjectURL(chibiResult.bytes);
+              dataList.appendChild(suggestion);
+            });
+          nameInput.appendChild(dataList);
+
+          document
+            .getElementsByClassName(
+              "navbar navbar-expand-md navbar-light bg-light"
+            )[0]
+            .appendChild(nameInput);
+          nameInput.addEventListener("change", (event: any) => {
+            console.log(event.target.value);
+            setCurrentChick(event.target.value);
+          });
+
+          const saveButton = document.createElement("button");
+          saveButton.id = "saveBtn";
+          saveButton.textContent = "save";
+          saveButton.onclick = () => {
+            console.log(chickRef.current, chickRef.current?.getArtwork());
+            const flat = binaryStringToByteArray(
+              chickRef.current?.getArtwork().getFlatPNG()
+            );
+            // save image file (for game engine)
+            const flatBlob = new Blob([flat], { type: "image/png" });
+            const blobUrl = URL.createObjectURL(flatBlob);
             storeCache(
               (nameInput.value || nameInput.placeholder || "chickenPaint") +
-                ".chi",
+                ".png",
               blobUrl
             );
+
+            console.log(
+              chickRef.current?.getArtwork(),
+              chickRef.current?.artwork
+                .getLayersRoot()
+                .getLinearizedLayerList(false)
+            );
+            // save chi file too
+            save(chickRef.current?.getArtwork()).then((chibiResult: any) => {
+              console.log("Layered saved:", chibiResult);
+              const blobUrl = URL.createObjectURL(chibiResult.bytes);
+              storeCache(
+                (nameInput.value || nameInput.placeholder || "chickenPaint") +
+                  ".chi",
+                blobUrl
+              );
+            });
+          };
+          document
+            .getElementsByClassName(
+              "navbar navbar-expand-md navbar-light bg-light"
+            )[0]
+            .appendChild(saveButton);
+          console.log(chickRef.current);
+
+          // we could also do this on input and add it to input's autocompletion
+
+          // restore chick Positions
+          // console.log("STYLES LOADED", chickWidgetPositions);
+          Object.entries(chickWidgetPositions).forEach(([key, value]: any) => {
+            //@ts-ignore
+            document.getElementsByClassName(key)[0].style = value;
           });
-        };
-        document
-          .getElementsByClassName(
-            "navbar navbar-expand-md navbar-light bg-light"
-          )[0]
-          .appendChild(saveButton);
-        console.log(chickRef.current);
-
-        // we could also do this on input and add it to input's autocompletion
-
-        // restore chick Positions
-        // console.log("STYLES LOADED", chickWidgetPositions);
-        Object.entries(chickWidgetPositions).forEach(([key, value]: any) => {
-          //@ts-ignore
-          document.getElementsByClassName(key)[0].style = value;
-        });
-      }, 100);
+        }, 100);
+      });
     }, 100);
   };
 
